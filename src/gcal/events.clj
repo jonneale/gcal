133d47158bd73550f3983950e34e47f8b22f701b
owain lewis <owain@owainlewis.com>
2013-03-29 10:32:58 +0000

    cleanup

--- src/gcal/events.clj
+++ src/gcal/events.clj
@@ -4,9 +4,7 @@
 (defn all
   "Returns a list of all events"
   [token id]
-  (api/simple-request (format "/calendars/%s/events" (java.net.URLDecoder/decode id)) token))
+  (api/simple-request
+    (format "/calendars/%s/events"
+      (java.net.URLDecoder/decode id)) token))
 
-;; (defn quick-add [calendar]
-;;   ;; https://www.googleapis.com/calendar/v3/calendars/calendarId/events/quickAdd
-;;   (let [t "Appointment at Somewhere on Februray 29th 10am-10:25am"]
-;;     (post-request purl)))
